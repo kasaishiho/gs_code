ade7901f14491e5453a6f56fae786c4eb8429253
kasaishiho <s1318116@cst.otsuma.ac.jp>
2020-06-08 09:10:29 +0900

    課題1回め（新）

---
+++ php02_ new/explode.php
@@ -0,0 +1,10 @@
+<?php
+$a="A,B,c";
+$b=explode(",",$a);
+
+var_dump($b);
+
+
+
+
+?>
---
+++ php02_ new/str_replace.php
@@ -0,0 +1,14 @@
+<?php
+$a="PHP5_PHP6_PHP7_PHP8";
+
+$b=str_replace("PHP","JS",$a);
+echo $b;
+
+
+
+
+
+
+
+
+?>
